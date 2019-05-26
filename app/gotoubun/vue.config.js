42fd088eba0ec221ef0ffaafe78c1e9faf888284
yokrh <y.oka.gml@gmail.com>
2019-05-27 02:18:35 +0900

    modify workbox option

--- app/gotoubun/vue.config.js
+++ app/gotoubun/vue.config.js
@@ -1,27 +1,46 @@
 module.exports = {
   pwa: {
     workboxOptions: {
-      // Exclude images from the precache
-      exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+      // Exclude from the precache
+      exclude: [/\.(?:png|jpg|jpeg|svg|json)$/],
 
       // Define runtime caching rules.
-      runtimeCaching: [{
-        // Match any request ends with .png, .jpg, .jpeg or .svg.
-        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-
-        // Apply a cache-first strategy.
-        handler: 'cacheFirst',
-
-        options: {
-          // Use a custom cache name.
-          cacheName: 'images',
-
-          // Only cache 10 images.
-          expiration: {
-            maxAgeSeconds: 30 * 24 * 60 * 60,
+      runtimeCaching: [
+        {
+          urlPattern: /data\/.*\.json$/,
+          handler: 'networkFirst',
+          options: {
+            cacheName: 'datajson',
+            expiration: {
+              maxAgeSeconds: 30 * 24 * 60 * 60,
+            },
           },
         },
-      }],
+        {
+          urlPattern: /\.(?:json)$/,
+          handler: 'cacheFirst',
+          options: {
+            cacheName: 'json',
+            expiration: {
+              maxAgeSeconds: 30 * 24 * 60 * 60,
+            },
+          },
+        },
+        {
+          // Match any request ends with .png, .jpg, .jpeg or .svg.
+          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+          // Apply a cache-first strategy.
+          handler: 'cacheFirst',
+          options: {
+            // Use a custom cache name.
+            cacheName: 'images',
+            // Only cache 30 days.
+            expiration: {
+              maxAgeSeconds: 30 * 24 * 60 * 60,
+            },
+          },
+        },
+      ],
     }
   }
 }

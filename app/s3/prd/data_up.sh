52d2031b90007c9ececa019685f2058c094e3407
yoshi <yoshi.okada@baozun.com>
2020-02-26 11:33:49 +0900

    update aws command to aws2

--- app/s3/prd/data_up.sh
+++ app/s3/prd/data_up.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-aws s3 sync ../../gotoubun/public/data/ s3://gotoubun/data/ --include "*.json"
-aws s3 sync ../../gotoubun/public/data/ s3://gotoubun/data/ --include "*.jpg"
+# aws s3 sync ../../gotoubun/public/data/ s3://gotoubun/data/ --include "*.json"
+# aws s3 sync ../../gotoubun/public/data/ s3://gotoubun/data/ --include "*.jpg"
+aws2 s3 sync ../../gotoubun/public/data/ s3://gotoubun/data/ --include "*.json"
+aws2 s3 sync ../../gotoubun/public/data/ s3://gotoubun/data/ --include "*.jpg"
